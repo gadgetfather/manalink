4727e10980169ffe86201f70bcbeb8196edebeec
Akshay Kamble <kamblea635@gmail.com>
2022-05-09 21:36:02 +0530

    update bg-img class

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,6 @@ module.exports = {
     fontFamily: {
       body: ["Radio Canada", "sans-serif"],
     },
-    backgroundImage: {
-      "background-pattern":
-        "url('https://images.unsplash.com/photo-1587095951604-b9d924a3fda0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80')",
-    },
   },
   plugins: [],
 };
