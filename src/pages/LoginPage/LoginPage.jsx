b966a191747ca852000d5a010af1ced32c08bdce
Akshay Kamble <kamblea635@gmail.com>
2022-05-09 21:33:29 +0530

    add create account button

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export function LoginPage() {
   return (
-    <div className="w-full min-h-screen bg-white flex justify-center items-center font-body tint-image">
-      <div className="bg-gray-300 p-4 rounded-md">
+    <div className="w-full min-h-screen  flex justify-center items-center font-body tint-image">
+      <div className="bg-gray-300 p-4 rounded-md lg:w-80">
         <h1 className="mb-3 font-body font-bold text-2xl font">
           Log in to Manalink
         </h1>
@@ -22,6 +22,13 @@ export function LoginPage() {
           <button className="rounded-md bg-slate-800 text-blue-50 mt-2 p-2  ">
             Login
           </button>
+          <span className="text-sm self-center mt-2">
+            Join us today
+            <span className="text-blue-600 underline cursor-pointer">
+              {" "}
+              Sign up
+            </span>
+          </span>
         </form>
       </div>
     </div>
