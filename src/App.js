716b19644b72ea5216b120ba267c7ada541c3a73
Akshay Kamble <kamblea635@gmail.com>
2022-05-10 22:12:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import { HomePage, LoginPage, SignupPage } from "./pages";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { ProtectedRoute } from "./routes/ProtectedRoute";
+import { useEffect } from "react";
+import { useAuth } from "./context/auth-context";
 function App() {
+  const {
+    userInfo: { token },
+  } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (token) {
+      navigate("/home");
+    } else {
+      navigate("/");
+    }
+  }, [token]);
   return (
     <div>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/home" element={<HomePage />} />
+
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
   );
