f248474edce62bfef27421e4737571ca567614bf
Akshay Kamble <kamblea635@gmail.com>
2022-05-10 22:04:13 +0530

    add logout button

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/auth-context";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export function HomePage() {
+  const { setUserInfo } = useAuth();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    setTimeout(() => {
+      localStorage.removeItem("manalink.user");
+      localStorage.removeItem("manalink.token");
+      setUserInfo({ token: "", user: "" });
+    }, 2000);
+
+    toast.error("You have been logged out", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
   return (
     <div>
       <h1>Homepage</h1>
-      <button>Logout</button>
+      <ToastContainer />
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
