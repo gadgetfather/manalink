d702f730f9edb7fd3311371bbcf034c8873f40af
Akshay Kamble <kamblea635@gmail.com>
2022-05-11 20:42:56 +0530

    update homePage decision

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,13 +2,41 @@ import React from "react";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { MobileMenu, Navbar } from "../../components";
+import {
+  MobileMenu,
+  Navbar,
+  PersonCard,
+  PostCard,
+  Sidebar,
+} from "../../components";
 
 export function HomePage() {
   return (
-    <div className="min-h-screen bg-gray-500">
-      <ToastContainer />
+    <div className="lg:w-[80%] mx-auto flex flex-col">
       <Navbar />
+      <ToastContainer />
+
+      <div className="min-h-screen grid grid-cols-1   lg:grid-layout">
+        <Sidebar />
+        <div>
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+        </div>
+        <div className="border flex flex-col  sticky top-[48px] h-[calc(100vh_-_48px)] gap-4 pt-2 border-r ">
+          <PersonCard />
+          <PersonCard />
+          <PersonCard />
+        </div>
+      </div>
       <MobileMenu />
     </div>
   );
