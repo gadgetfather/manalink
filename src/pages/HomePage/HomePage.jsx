55a41716a3661e98be535eaf3cddccadb07a2bba
Akshay Kamble <kamblea635@gmail.com>
2022-05-11 12:07:16 +0530

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Navbar } from "../../components";
+import { MobileMenu, Navbar } from "../../components";
 
 export function HomePage() {
   return (
     <div className="min-h-screen bg-gray-500">
       <ToastContainer />
       <Navbar />
+      <MobileMenu />
     </div>
   );
 }
