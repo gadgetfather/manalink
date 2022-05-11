a853fef03eb0fe0b8afe8dcd5c0d1a1c0ca873d5
Akshay Kamble <kamblea635@gmail.com>
2022-05-11 11:43:41 +0530

    moved logout function to navbar

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,32 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/auth-context";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Navbar } from "../../components";
 
 export function HomePage() {
-  const { setUserInfo } = useAuth();
-  const navigate = useNavigate();
-  const handleLogout = () => {
-    setTimeout(() => {
-      localStorage.removeItem("manalink.user");
-      localStorage.removeItem("manalink.token");
-      setUserInfo({ token: "", user: "" });
-    }, 2000);
-
-    toast.error("You have been logged out", {
-      position: "top-center",
-      autoClose: 2000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-  };
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="min-h-screen bg-gray-500">
       <ToastContainer />
-      <button onClick={handleLogout}>Logout</button>
+      <Navbar />
     </div>
   );
 }
