170b12a0b762df7ccfde1ef685c5ee5bb7d1cfc0
Akshay Kamble <kamblea635@gmail.com>
2022-05-14 13:16:20 +0530

    fix : render bug

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/auth-context";
+import { createRoot } from "react-dom/client";
 // Call make Server
 makeServer();
-
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
