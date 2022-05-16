2a9cf5af9a5567403365ab59817a31c69182d118
Akshay Kamble <kamblea635@gmail.com>
2022-05-16 10:46:31 +0530

    provided redux store

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/auth-context";
+// import { AuthProvider } from "./context/auth-context";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 // Call make Server
 makeServer();
 const rootElement = document.getElementById("root");
@@ -12,9 +14,9 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
+      <Provider store={store}>
         <App />
-      </AuthProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
