1e74af254c16377505f0e50d808381a87185199c
Akshay Kamble <kamblea635@gmail.com>
2022-05-17 15:14:05 +0530

    Update posts on bookmark page

--- src/pages/BookmarkPage/BookmarkPage.jsx
+++ src/pages/BookmarkPage/BookmarkPage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import { MobileMenu, Navbar, PostCard, Sidebar } from "../../components";
 
 export function BookmarkPage() {
+  const bookmarks = useSelector((state) => state.post.bookmarks);
+
   return (
     <div className="lg:w-[80%] xl:w-[70%] 2xl:w-[60%] mx-auto flex flex-col">
       <Navbar />
@@ -14,10 +17,11 @@ export function BookmarkPage() {
           <h1 className="text-3xl mb-2 text-center font-semibold">
             Saved Posts
           </h1>
-          {/* <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard /> */}
+          {bookmarks.length > 0 ? (
+            bookmarks.map((post) => <PostCard key={post.id} {...post} />)
+          ) : (
+            <h1 className="text-center mt-4 text-lg">Nothing is here ...</h1>
+          )}
         </div>
       </div>
       <MobileMenu />
