6d2f468d29f63e9e15e310fd538ba0e28fd6165d
Akshay Kamble <kamblea635@gmail.com>
2022-05-17 13:22:23 +0530

    add like functionality to postCard

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,17 +1,29 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { dislikePost, likePost } from "../../redux/features/post/postThunk";
 
 export function PostCard(props) {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { username: currentUser } = useSelector((state) => state.auth.user);
   const {
     _id,
     content,
     username,
-    likes: { likeCount },
+    likes: { likeCount, likedBy },
   } = props;
   const handleNavigateToProfile = () => {
     navigate(`/${username}`);
   };
+  const handleLike = (id) => {
+    dispatch(likePost(id));
+  };
+
+  const handleDislike = (id) => {
+    dispatch(dislikePost(id));
+  };
+
   return (
     <div className=" p-2   flex flex-col border-b    ">
       <div className="flex items-center gap-2">
@@ -23,13 +35,25 @@ export function PostCard(props) {
         <h2 onClick={handleNavigateToProfile}>{username}</h2>
       </div>
       <div className="text-base">
-        <p className="whitespace-pre-line">{content}</p>
+        <p className="whitespace-pre-line py-2">{content}</p>
       </div>
       <div className="pt-2 flex justify-between">
         <div className="flex items-center justify-center gap-2">
-          <span className="cursor-pointer material-symbols-outlined">
-            favorite
-          </span>
+          {likedBy.some((user) => user.username === currentUser) ? (
+            <span
+              onClick={() => handleDislike(_id)}
+              className="cursor-pointer material-symbols-outlined heartFilled text-red-500"
+            >
+              favorite
+            </span>
+          ) : (
+            <span
+              onClick={() => handleLike(_id)}
+              className="cursor-pointer material-symbols-outlined"
+            >
+              favorite
+            </span>
+          )}
           {likeCount}
         </div>
         <span className="cursor-pointer material-symbols-outlined">chat</span>
