78cdd354f0efcd6b2704f28240e79ced0656f589
Akshay Kamble <kamblea635@gmail.com>
2022-05-19 10:21:55 +0530

    added comments and post-related functions

--- src/redux/features/post/postSlice.js
+++ src/redux/features/post/postSlice.js
@@ -9,18 +9,33 @@ import {
   likePost,
   addToBookmarks,
   removeFromBookmarks,
+  addComment,
+  getComments,
+  upvoteComment,
+  downvoteComment,
+  editComment,
+  deleteComment,
+  deletePost,
+  editPost,
 } from "./postThunk";
 const initialState = {
   posts: [],
   loading: false,
   userPost: [],
   bookmarks: [],
+  singlePost: "",
+  comments: [],
 };
 
 const postSlice = createSlice({
   name: "post",
   initialState,
-  reducers: {},
+  reducers: {
+    setSinglePost: (state, action) => {
+      // console.log("action", action.payload[0]);
+      state.singlePost = action.payload[0];
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(getPosts.pending, (state, action) => {
@@ -68,8 +83,58 @@ const postSlice = createSlice({
       })
       .addCase(removeFromBookmarks.rejected, (state, action) => {
         toastError(action.payload.errors[0]);
+      })
+      .addCase(addComment.fulfilled, (state, action) => {
+        state.comments = action.payload.comments;
+      })
+      .addCase(addComment.rejected, (state, action) => {
+        toastError(action.payload.errors[0]);
+      })
+      .addCase(getComments.fulfilled, (state, action) => {
+        state.comments = action.payload.comments;
+      })
+      .addCase(getComments.rejected, (state, action) => {
+        toastError(action.payload.errors[0]);
+      })
+      .addCase(upvoteComment.fulfilled, (state, action) => {
+        state.comments = action.payload.comments;
+      })
+      .addCase(upvoteComment.rejected, (state, action) => {
+        toastError(action.payload.errors[0]);
+      })
+      .addCase(downvoteComment.fulfilled, (state, action) => {
+        state.comments = action.payload.comments;
+      })
+      .addCase(downvoteComment.rejected, (state, action) => {
+        toastError(action.payload.errors[0]);
+      })
+      .addCase(editComment.fulfilled, (state, action) => {
+        state.comments = action.payload.comments;
+      })
+      .addCase(editComment.rejected, (state, action) => {
+        toastError(action.payload.errors[0]);
+      })
+      .addCase(deleteComment.fulfilled, (state, action) => {
+        state.comments = action.payload.comments;
+        toastError("Comment Has been deleted");
+      })
+      .addCase(deleteComment.rejected, (state, action) => {
+        toastError(action.payload.errors[0]);
+      })
+      .addCase(deletePost.fulfilled, (state, action) => {
+        state.posts = action.payload.posts;
+        toastError("Your post has been deleted");
+      })
+      .addCase(deletePost.rejected, (state, action) => {
+        toastError(action.payload.message);
+      })
+      .addCase(editPost.fulfilled, (state, action) => {
+        state.posts = action.payload.posts;
+      })
+      .addCase(editPost.rejected, (state, action) => {
+        toastError(action.payload.errors[0]);
       });
   },
 });
-
+export const { setSinglePost } = postSlice.actions;
 export default postSlice.reducer;
