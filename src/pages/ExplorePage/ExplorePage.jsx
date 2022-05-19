4b9c7fdb7914f7e8d5c5ad413023dfac76f95f2e
Akshay Kamble <kamblea635@gmail.com>
2022-05-19 23:44:43 +0530

    updated posts on explore page

--- src/pages/ExplorePage/ExplorePage.jsx
+++ src/pages/ExplorePage/ExplorePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import {
   MobileMenu,
@@ -9,6 +10,8 @@ import {
 } from "../../components";
 
 export function ExplorePage() {
+  const { posts } = useSelector((state) => state.post);
+  console.log(posts);
   return (
     <div className="lg:w-[80%] xl:w-[70%] 2xl:w-[60%] mx-auto flex flex-col">
       <Navbar />
@@ -20,17 +23,9 @@ export function ExplorePage() {
           <h1 className="text-center text-3xl font-bold mb-2">
             Explore New Things...
           </h1>
-          {/* <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard /> */}
+          {posts.map((post) => (
+            <PostCard key={post.id} {...post} />
+          ))}
         </div>
         <div className="hidden border lg:flex flex-col  sticky top-[48px] h-[calc(100vh_-_48px)] gap-4 pt-2 border-r pb-3">
           <PersonCard />
