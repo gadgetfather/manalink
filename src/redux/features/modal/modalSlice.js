62aadd7522be2103e0d0f95651854d8bcf6d718e
Akshay Kamble <kamblea635@gmail.com>
2022-05-19 10:24:43 +0530

    update modal functionalities

--- src/redux/features/modal/modalSlice.js
+++ src/redux/features/modal/modalSlice.js
@@ -1,6 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   isOpen: false,
+  editCommentModal: false,
+  editCommentText: "",
+  editCommentId: "",
+  editPostId: "",
+  editPostText: "",
+  editPostModal: false,
 };
 const modalSlice = createSlice({
   name: "modal",
@@ -14,7 +20,39 @@ const modalSlice = createSlice({
       state.isOpen = false;
       document.body.style.overflow = "unset";
     },
+    openEditCommentModal: (state, action) => {
+      state.editCommentModal = true;
+      state.editCommentText = action.payload.text;
+      state.editCommentId = action.payload._id;
+
+      document.body.style.overflow = "hidden";
+    },
+    closeEditCommentModal: (state, action) => {
+      state.editCommentModal = false;
+      document.body.style.overflow = "unset";
+      state.editCommentText = "";
+      state.editCommentId = "";
+    },
+    openEditPostModal: (state, action) => {
+      state.editPostModal = true;
+      document.body.style.overflow = "hidden";
+      state.editPostId = action.payload.postId;
+      state.editPostText = action.payload.text;
+    },
+    closeEditPostModal: (state, action) => {
+      state.editPostModal = false;
+      document.body.style.overflow = "unset";
+      state.editCommentText = "";
+      state.editCommentId = "";
+    },
   },
 });
-export const { openModal, closeModal } = modalSlice.actions;
+export const {
+  openModal,
+  closeModal,
+  openEditCommentModal,
+  closeEditCommentModal,
+  openEditPostModal,
+  closeEditPostModal,
+} = modalSlice.actions;
 export default modalSlice.reducer;
