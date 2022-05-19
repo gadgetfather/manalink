a21aeb84a394efd7a9ac3697ff6e8e5ae2031102
Akshay Kamble <kamblea635@gmail.com>
2022-05-20 00:01:12 +0530

    update store with userReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import modalReducer from "./features/modal/modalSlice";
 import postReducer from "./features/post/postSlice";
+import userReducer from "./features/user/userSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     modal: modalReducer,
     post: postReducer,
+    user: userReducer,
   },
 });
