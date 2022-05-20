e1842566d84aeb5096c36d3ed87304ac47867bde
Akshay Kamble <kamblea635@gmail.com>
2022-05-21 01:12:20 +0530

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
     extend: {
       colors: {
         "custom-yellow": "#BAA333",
-        "primary-orange": "#FF7700",
+        "primary-orange": {
+          100: "#FFF67F",
+          200: "#FFDD66",
+          300: "#FFC44D",
+          400: "#FFAA33",
+          500: "#FF911A",
+          600: "#FF7700",
+          700: "#E65E00",
+          800: "#CC4400",
+          900: "#B32B00",
+        },
       },
     },
   },
