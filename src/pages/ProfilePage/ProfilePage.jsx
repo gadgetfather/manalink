8a6179b61376d2fdcc869e8de62d37e5ee4dac55
Akshay Kamble <kamblea635@gmail.com>
2022-05-20 10:30:20 +0530

    update profilePage
    made it dynamic, will render according to user

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -3,16 +3,29 @@ import { ToastContainer } from "react-toastify";
 import { useState } from "react";
 import { MobileMenu, Navbar, PostCard, Sidebar } from "../../components";
 import { useDispatch, useSelector } from "react-redux";
-import { getUserPost } from "../../redux/features/post/postThunk";
+
 import { useParams } from "react-router-dom";
+import { getSingleUser } from "../../redux/features/user/userThunk";
+import { getPosts } from "../../redux/features/post/postThunk";
 
 export function ProfilePage() {
   const { profile } = useParams();
-  const { userPost } = useSelector((state) => state.post);
+  const { posts } = useSelector((state) => state.post);
+  const {
+    user: { username: currentUser },
+  } = useSelector((state) => state.auth);
+
+  const {
+    visitingUser: { username },
+  } = useSelector((state) => state.user);
+
   const [showHover, setShowHover] = useState(false);
+  const userPosts = posts.filter((post) => post.username === profile).reverse();
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getUserPost(profile));
+    dispatch(getPosts());
+
+    dispatch(getSingleUser(profile));
   }, [dispatch, profile]);
   return (
     <div className="lg:w-[80%] xl:w-[70%] 2xl:w-[60%] mx-auto flex flex-col ">
@@ -34,29 +47,37 @@ export function ProfilePage() {
                   src="https://picsum.photos/200"
                   alt=""
                 />
-                <div
-                  className={` absolute ${
-                    showHover ? "flex" : "hidden"
-                  } items-center justify-center left-0 right-0 top-0 bottom-0 text-sm bg-sky-500/50 rounded-full cursor-pointer z-20`}
-                >
-                  <label className="cursor-pointer" htmlFor="image">
-                    <span className="material-symbols-outlined">
-                      photo_camera
-                    </span>
-                  </label>
-                  <input id="image" className="z-50 hidden" type="file" />
-                </div>
+                {profile === currentUser ? (
+                  <div
+                    className={` absolute ${
+                      showHover ? "flex" : "hidden"
+                    } items-center justify-center left-0 right-0 top-0 bottom-0 text-sm bg-sky-500/50 rounded-full cursor-pointer z-20`}
+                  >
+                    <label className="cursor-pointer" htmlFor="image">
+                      <span className="material-symbols-outlined">
+                        photo_camera
+                      </span>
+                    </label>
+                    <input id="image" className="z-50 hidden" type="file" />
+                  </div>
+                ) : (
+                  ""
+                )}
               </div>
 
-              <h1 className="font-medium text-lg">UserName</h1>
+              <h1 className="font-medium text-lg">{username}</h1>
             </div>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Asperiores iste corporis repellat expedita tempora excepturi
             </p>
-            <button className="ml-auto mr-2">Edit</button>
+            {profile === currentUser ? (
+              <button className="ml-auto mr-2">Edit</button>
+            ) : (
+              <button className="ml-auto mr-2">follow</button>
+            )}
           </div>
-          {userPost.map((post) => (
+          {userPosts.map((post) => (
             <PostCard key={post.id} {...post} />
           ))}
         </div>
