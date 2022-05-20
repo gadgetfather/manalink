d5594962fb9b4b57ab84797ab5981ac61dd1ffaa
Akshay Kamble <kamblea635@gmail.com>
2022-05-20 12:17:20 +0530

    update person card
    Now can navigate to user profile from it

--- src/components/PersonCard/PersonCard.jsx
+++ src/components/PersonCard/PersonCard.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { follow, unfollow } from "../../redux/features/user/userThunk";
 
 export function PersonCard(props) {
+  const navigate = useNavigate();
   const { username, _id } = props;
   const { user } = useSelector((state) => state.auth);
 
   const dispatch = useDispatch();
-  const handleFollow = () => {
+  const handleFollow = (e) => {
+    e.stopPropagation();
     dispatch(follow(_id));
   };
-  const handleUnfollow = () => {
+  const handleUnfollow = (e) => {
+    e.stopPropagation();
     dispatch(unfollow(_id));
+  };
+  const navigateToProfile = () => {
+    navigate(`/${username}`);
   };
   return (
     <>
       {username !== user.username ? (
-        <div className=" flex flex-col p-2 border-b">
+        <div
+          onClick={navigateToProfile}
+          className="cursor-pointer flex flex-col p-2 border-b"
+        >
           <div className="flex items-center gap-2">
             <img
               className="w-10 h-10 rounded-full"
@@ -27,9 +37,9 @@ export function PersonCard(props) {
           </div>
           <div className="mt-2 ml-auto mr-4">
             {user.following.some((user) => user.username === username) ? (
-              <button onClick={handleUnfollow}>Following</button>
+              <button onClick={(e) => handleUnfollow(e)}>Following</button>
             ) : (
-              <button onClick={handleFollow}>Follow</button>
+              <button onClick={(e) => handleFollow(e)}>Follow</button>
             )}
           </div>
         </div>
